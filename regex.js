652ad61cefbf8d87692435b064dfdbd818b3a7e3
tronixashutrosh <56515701+tronixashutrosh@users.noreply.github.com>
2022-09-30 13:17:27 +0530

    Update regex.js

--- regex.js
+++ regex.js
@@ -1,7 +1,8 @@
 // length constraint ,already fixed the first character, so we give {7,29}.
-// We use ^ and $ to specify the beginning and end of matching. 
+// We use ^ and $ to specify the beginning and end of matching and will make sure that nothing but these alphabets will be matched.
 // We use * to match all.
 
+const validateName = /^[A-Za-z]+$/
 const validateFloat = /^[0-9]*(\.[0-9]+)*$/
 const validateNumber = /^[0-9]*$/
 const validateEmail = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
